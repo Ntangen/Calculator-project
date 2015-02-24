6b74c7a7f8db2b35267254e038ad8d4fcc1ba0e7
J. Blair Reeves <Blair@Blairs-Air.local>
2015-02-24 08:18:55 -0500

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by J. Blair Reeves, Jr. on 2/24/15.
+//  Copyright (c) 2015 J. Blair Reeves, Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
