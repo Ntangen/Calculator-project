fffa11bba7f0b390d1466ce911127ba4753fdde1
Blair Reeves <gaslight@gmail.com>
2015-04-07 13:00:06 -0400

    Pre-assignment 2

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userTyping = false
+    let x = M_PI
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userTyping{
+        display.text = display.text! + digit
+        } else {
+        display.text = digit
+        userTyping = true
+        }
+        
+    }
+    
+    @IBAction func backspace(sender: UIButton) {
+        display.text = dropLast(display.text!)
+    }
+    
+    @IBAction func pi(){
+        display.text = "\(x)"
+        userTyping = true
+    }
+   
+    
+    @IBAction func clear(sender: UIButton) {
+        display.text = "0"
+        history.text = "0"
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userTyping {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+         }
+    }
+    
+    @IBAction func enter() {
+        userTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        }
+        else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userTyping = false
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var history: UILabel!
 
+    @IBAction func appendDigit2(sender: UIButton) {
+        let digit = sender.currentTitle!
+        history.text = history.text! + digit
+    }
 
 }
-
